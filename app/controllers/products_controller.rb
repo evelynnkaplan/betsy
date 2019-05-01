a277abbb91887e8ddbb885b4c2ccf4602a4c642f
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-01 11:29:06 -0700

    wrote create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,22 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product_hash = {
+      description: params[:description],
+      name: params[:name],
+      price: params[:price],
+      stock: params[:stock],
+      img_url: params[:img_url],
+      # take a second look at this later
+      merchant_id: params[:merchant_id] ||= Merchant.find_by(username: params[:merchant]),
+    }
+
+    product = Product.new(product_hash)
+  end
 end
