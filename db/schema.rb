e0ea5bd1499acd6eb2b4e319c49df36d699bdd7c
Erica Norris <norrise120@gmail.com>
2019-05-01 10:59:48 -0700

    regenerate merchant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_172914) do
+ActiveRecord::Schema.define(version: 2019_05_01_175621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
