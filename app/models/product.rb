7bb20a1cee4c6c41b8b93f26b81729be555b2797
Tatiana Quintana <tatsqui@gmail.com>
2019-05-01 15:21:09 -0700

    wrote tests for validating price greater than zero, and instantiation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :merchant
+  validates :name, uniqueness: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,67 @@
 require "test_helper"
 
 describe Product do
-  let(:product) { Product.new }
+  let(:product) { products(:product_one)}
 
-  it "must be valid" do
-    value(product).must_be :valid?
+  describe "instantiation" do 
+    it "can be instantiated" do
+      
+      expect(product.valid?).must_equal true
+    end
+
+    it "has all attributes" do
+      product_attribute_array.each do |field|
+        expect(product).must_respond_to field
+      end
+    end
+
+    it "validates present and unique names" do 
+      # Arrange
+      nil_product = Product.new(name: nil)
+      empty_name = Product.new(name: "")
+
+      # Assert presence
+      expect(nil_product.valid?).must_equal false
+      expect(empty_name.valid?).must_equal false
+
+      # Arrange & Assert uniqueness
+      used_name = product.name
+      new_product = Product.new(name: used_name)
+
+      expect(new_product.valid?).must_equal false
+    end
+
+    it "validates price is present and greater than zero" do 
+      #Arrange edge cases
+      product.price = 0
+      result = product.save
+
+      #Assert
+      expect(result).must_equal false
+
+      # Arrange and Act
+      product.price = "A"
+      string_result = product.save
+      
+      expect(string_result).must_equal false
+
+      # Arrange & Act
+      product.price = 100
+      valid_result = product.save
+      
+      # nominal case
+      expect(valid_result).must_equal true
+    end
+  end
+
+  describe "relationships" do 
+    it "must belong to a merchant" do 
+    end
+
+    it "has one or many categories" do 
+    end
+  end
+
+  describe "custom methods" do 
   end
 end
