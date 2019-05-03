93eaf934291b8b02ede8dda2c07358834b402a1e
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-03 14:18:29 -0700

    added merchants resources and dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "merchants#index"
 
-  resources :merchants #show is restricted because it serves as the merchant dashboard
-  # /merchants/:id/products <- Merchant's shop
-  
+  resources :merchants
+  get "/dashboard", to: "merchants#show", as: "dashboard"
+
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
   delete "/logout", to: "merchants#destroy", as: "logout"
