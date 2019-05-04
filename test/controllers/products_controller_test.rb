82b8678a0f43fa63016cc778e16cd3da499a7e34
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-04 15:44:04 -0700

    added check_flash to tests and wrote update and destroy tests for logged in users

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -21,6 +21,7 @@ describe ProductsController do
 
       must_respond_with :redirect
       must_redirect_to products_path
+      check_flash(:error)
     end
   end
 
@@ -53,6 +54,7 @@ describe ProductsController do
 
         must_respond_with :redirect
         must_redirect_to products_path
+        check_flash
       end
 
       it "won't create a new product with bad data and will redirect" do
@@ -65,6 +67,7 @@ describe ProductsController do
 
         must_respond_with :redirect
         must_redirect_to products_path
+        check_flash(:error)
       end
     end
 
@@ -80,6 +83,7 @@ describe ProductsController do
 
         must_respond_with :redirect
         must_redirect_to products_path
+        check_flash(:error)
       end
     end
 
@@ -99,6 +103,43 @@ describe ProductsController do
         test_product.reload
 
         expect(test_product.name).must_equal product_params[:product][:name]
+        check_flash
+      end
+
+      it "will redirect if given a bad product ID" do
+        patch product_path(-20)
+
+        must_respond_with :redirect
+        must_redirect_to products_path
+        check_flash(:error)
+      end
+    end
+
+    describe "destroy" do
+      it "can destroy an existing product" do
+        test_product = Product.new(name: "test",
+                                   price: 5,
+                                   merchant_id: Merchant.first.id)
+
+        expect { test_product.save }.must_change "Product.count", +1
+
+        expect {
+          delete product_path(test_product.id)
+        }.must_change "Product.count", -1
+
+        must_respond_with :redirect
+        must_redirect_to products_path
+        check_flash
+      end
+
+      it "won't destroy a product if given a bad product ID" do
+        expect {
+          delete product_path(-15)
+        }.wont_change "Product.count"
+
+        must_respond_with :redirect
+        must_redirect_to products_path
+        check_flash(:error)
       end
     end
   end
