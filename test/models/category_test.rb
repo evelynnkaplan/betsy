c7922bbf04be046c5d7b7e33cd8c4a7bb7870593
Tatiana Quintana <tatsqui@gmail.com>
2019-05-04 18:10:08 -0700

    added validation test to category

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,18 @@
 require "test_helper"
 
 describe Category do
-  let(:category) { Category.new }
+  let(:category) { Category.new(name: "Taco Cat") }
 
-  it "must be valid" do
-    value(category).must_be :valid?
+  describe "validations" do 
+    it "must be valid" do
+      value(category).must_be :valid?
+    end
+
+    it "doesn't allow repeat names" do 
+      category.save
+      new_categ = Category.new(name: "Taco Cat")
+
+      expect(new_categ.valid?).must_equal false
+    end
   end
 end
