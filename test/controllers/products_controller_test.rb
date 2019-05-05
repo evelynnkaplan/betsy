3c6b9757d2cc7133aff82c73790c2fd943e41f0e
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-04 17:35:59 -0700

    wrote tests for merchant_product_index and category_product_index

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,25 +3,58 @@ require "test_helper"
 describe ProductsController do
   let(:product) { Product.first }
 
-  it "can get the index page" do
-    get products_path
-
-    must_respond_with :ok
-  end
-
-  describe "show" do
-    it "can get the page for a product that exists" do
-      get product_path(product.id)
+  describe "browsing products for all users" do
+    it "can get the index page of all products" do
+      get products_path
 
       must_respond_with :ok
     end
 
-    it "will redirect when given a bad product id" do
-      get product_path(555555)
+    it "can get the page of products by merchant" do
+      merchant = Merchant.first
+      get merchant_products_path(merchant.id)
+
+      must_respond_with :ok
+    end
+
+    it "won't get the page of products by merchant when given a bad merchant id" do
+      get merchant_products_path(-23)
 
       must_respond_with :redirect
       must_redirect_to products_path
       check_flash(:error)
+    end
+
+    it "can get the page of products by category" do
+      category = Category.create!(name: "test")
+
+      get category_products_path(category.id)
+
+      must_respond_with :ok
+    end
+
+    it "won't get the page of products by category when given a bad category id" do
+      get category_products_path(-50)
+
+      must_respond_with :redirect
+      must_redirect_to products_path
+      check_flash(:error)
+    end
+
+    describe "show" do
+      it "can get the page for a product that exists" do
+        get product_path(product.id)
+
+        must_respond_with :ok
+      end
+
+      it "will redirect when given a bad product id" do
+        get product_path(555555)
+
+        must_respond_with :redirect
+        must_redirect_to products_path
+        check_flash(:error)
+      end
     end
   end
 
@@ -142,5 +175,8 @@ describe ProductsController do
         check_flash(:error)
       end
     end
+  end
+
+  describe "guest users" do
   end
 end
