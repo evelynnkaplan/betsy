9e7e1d86c4a702f37ec3d8c27c3443b3b4aa5bc8
Tatiana Quintana <tatsqui@gmail.com>
2019-05-06 10:43:16 -0700

    review test for creating new review

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -13,10 +13,22 @@ describe ReviewsController do
 
   describe "create" do 
     it "creates a new review successfully" do 
-      perform_login
-      current_merchant
+      review_data = {
+        review: {
+          rating: 5, 
+          comment: "The best information."
+        }
+      }
 
+      expect {
+        post product_reviews_path(product_id), params: review_data
+      }.must_change "Review.count", +1
+        
+      check_flash
       
+      must_respond_with :redirect
+      must_redirect_to product_path(product_id)
+
     end
 
     it "will not allow a merchant to review their own product" do 
