d4fcba75ae46a5e10191d98a715836259e286438
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-06 12:15:58 -0700

    uncommented order_items validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_items
-  # validates :order_items, presence: true
+  validates :order_items, presence: true
   validates :status, inclusion: {in: [nil, "pending", "paid", "complete", "canceled"]}
   before_save :update_total
   before_create :update_status
