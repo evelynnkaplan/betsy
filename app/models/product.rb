f07e8b132ca47e0d01b59084c48a837bc0bc66c3
Erica Norris <norrise120@gmail.com>
2019-05-06 14:10:46 -0700

    html and css for product show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,26 @@ class Product < ApplicationRecord
   has_and_belongs_to_many :categories
   belongs_to :merchant
   validates :name, presence: true, uniqueness: true
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: {greater_than: 0}
+
+  def related_products
+    related_products = []
+    if Product.count >= 6
+      until related_products.length == 5
+        product = Product.all.sample
+
+        if related_products.include?(product) == false && product != self
+          related_products.push(product)
+        end
+      end
+    else
+      Product.all.each do |product|
+        if product != self
+          related_products.push(product)
+        end
+      end
+    end
+
+    return related_products
+  end
 end
