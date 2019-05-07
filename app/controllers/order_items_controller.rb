c6808a522ed3dc64f0e9d6456a99cc00506cc97b
Shirley Chu <shirley.cchu@gmail.com>
2019-05-07 09:24:48 -0700

    order item test #1 passed

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,20 @@
+require "pry"
+
 class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @item = @order.order_items.new(item_params)
-    @order.save
-    session[:order_id] = @order.id
-    redirect_to products_path
+    if @order.save
+      flash[:status] = :success
+      flash[:message] = "Order #{@order.id} successfully created"
+
+      session[:order_id] = @order.id
+      redirect_to products_path
+    else
+      flash[:status] = :error
+      flash[:message] = "Order cannot be saved: #{@order.errors.messages}"
+      redirect_back(fallback_location: products_path)
+    end
   end
 
   def update
