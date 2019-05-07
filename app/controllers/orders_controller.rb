f759b60c870b2036d287e306f27fee67cf31f2e7
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-06 21:43:04 -0700

    wrote index action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,28 @@
 class OrdersController < ApplicationController
   def index
-
+    if session[:merchant_id]
+      @merchant = Merchant.find_by(id: session[:merchant_id])
+      @orders = @merchant.orders
+    else
+      flash[:status] = :error
+      flash[:message] = "You don't have permission to see a list of orders. Please log in."
+      redirect_to root_path
+    end
   end
-  
+
   def show
-
   end
-  
-  def create
 
+  def create
   end
 
   def edit
-    
   end
 
   def update
-
   end
 
   def view_cart
     @order_items = current_order.order_items
-  end 
-  
+  end
 end
