6a500d93f820dca63bcedb5dad49d3c985e4b3de
Tatiana Quintana <tatsqui@gmail.com>
2019-05-07 10:46:10 -0700

    finished product model relationship tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
   validates :name, uniqueness: true, presence: true
+  before_save :lowercase_name
+end
+
+def self.lowercase_name
+  return self.name.downcase!
 end
