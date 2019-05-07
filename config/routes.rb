12ec0a21a8daae364a1b08f750255f4f0b584d6b
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-07 11:06:23 -0700

    added checkout route, removed create

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,17 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
   delete "/logout", to: "merchants#destroy", as: "logout"
 
-  resources :categories, only: [:index, :new, :create, :show] 
+  resources :categories, only: [:index, :new, :create, :show]
   get "categories/:id/products", to: "products#category_product_index", as: "category_products"
- 
+
   resources :products do
     resources :reviews, only: [:new, :create]
   end
 
   get "orders/view_cart", to: "orders#view_cart", as: "view_cart"
+  get "orders/checkout", to: "orders#edit", as: "checkout"
   get "order/:id/confirmation", to: "orders#confirmation", as: "order_confirmation"
-  resources :orders, except: [:new, :destroy]
+  resources :orders, except: [:new, :destroy, :create]
 
   resources :order_items, only: [:create, :update, :destroy]
 end
