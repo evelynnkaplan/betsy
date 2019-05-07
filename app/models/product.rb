689820bf795fdd3acfe779f1b12ec46b7d0bfebd
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-05-06 21:42:46 -0700

    added has_many order_items relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews
+  has_many :order_items
   has_and_belongs_to_many :categories
   belongs_to :merchant
   validates :name, presence: true, uniqueness: true
