de104ac8f7f3a6bdffbac6ce29b2d571880af365
Tatiana Quintana <tatsqui@gmail.com>
2019-05-07 10:55:51 -0700

    wrote custom method test for lowercase name in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,9 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :products
   validates :name, uniqueness: true, presence: true
   before_save :lowercase_name
+
+  def lowercase_name
+    return self.name.downcase!
+  end
 end
 
-def self.lowercase_name
-  return self.name.downcase!
-end
