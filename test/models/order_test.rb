b3b9cc869576b421e1fa8eb4d69082f49aee447d
Tatiana Quintana <tatsqui@gmail.com>
2019-05-08 15:15:56 -0700

    changed credit_card to be bigint

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,38 +1,38 @@
 require "test_helper"
 
 describe Order do
-  # let(order) {orders(:cart_one)}
-  # describe "validations" do 
-  #   it "can be instantiated" do 
-  #     new_order = Order.new
+  let(:order) {orders(:cart_one)}
+  describe "validations" do 
+    it "can be instantiated" do 
+      new_order = Order.new
 
-  #     expect(new_order.valid?).must_equal true
-  #   end
+      expect(new_order.valid?).must_equal true
+    end
 
-  #   it "knows its fields" do 
-  #     order_attribute_array.each do |field|
-  #       expect(order).must_respond_to field
-  #     end
-  #   end
+    it "knows its fields" do 
+      order_attribute_array.each do |field|
+        expect(order).must_respond_to field
+      end
+    end
 
-  #   it "validates for multiple required fields" do 
-  #     new_order = Order.new
-  #     new_order.save
+    it "validates for multiple required fields" do 
+      new_order = Order.new
+      new_order.save
 
-  #     expect(new_order.errors.messages).must_include :name_on_card
-  #     expect(new_order.errors.messages).must_include :credit_card
-  #     expect(new_order.errors.messages).must_include :cvv
-  #     expect(new_order.errors.messages).must_include :billing_zip
-  #     expect(new_order.errors.messages).must_include :card_exp
-  #     expect(new_order.errors.messages).must_include :status
-  #   end
-  # end
+      expect(new_order.errors.messages).must_include :name_on_card
+      expect(new_order.errors.messages).must_include :credit_card
+      expect(new_order.errors.messages).must_include :cvv
+      expect(new_order.errors.messages).must_include :billing_zip
+      expect(new_order.errors.messages).must_include :card_exp
+      expect(new_order.errors.messages).must_include :status
+    end
+  end
 
-  # describe "relationships" do 
+  describe "relationships" do 
 
-  # end
+  end
   
-  # describe "custom methods" do 
+  describe "custom methods" do 
 
-  # end
+  end
 end
