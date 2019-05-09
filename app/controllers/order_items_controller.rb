b09f09e592f4650a31c7480c3214040d123f6dac
Shirley Chu <shirley.cchu@gmail.com>
2019-05-09 11:49:55 -0700

    beon is back, friends!

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,21 @@
 require "pry"
 
 class OrderItemsController < ApplicationController
-  
   def create
-    # an order is made, but not yet saved 
+    # an order is made, but not yet saved
     @order = current_order
 
-    # checks whether order item already exists in cart 
+    # checks whether order item already exists in cart
     @item = @order.order_items.find_by(product_id: item_params[:product_id])
-    
-    # updates quantity if orderitem already exists, otherwise creates new order item 
+
+    # updates quantity if orderitem already exists, otherwise creates new order item
     if @item
-      @item.quantity += item_params[:quantity].to_i 
-    else 
+      @item.quantity += item_params[:quantity].to_i
+    else
       @item = @order.order_items.new(item_params)
-    end 
-    
-    # save the order with the new item. each time an order item gets added, the order will be saved 
+    end
+
+    # save the order with the new item. each time an order item gets added, the order will be saved
     if @order.save
       flash[:status] = :success
       flash[:message] = "Item successfully added to cart"
@@ -32,30 +31,37 @@ class OrderItemsController < ApplicationController
 
   def update
     @order = current_order
-    @order_item = @order.order_items.find(params[:id])
-    @order_item.update_attributes(item_params)
-    @order_items = @order.order_items
-    flash[:status] = :success
-    flash[:message] = "Quantity successfully updated"
-    redirect_to view_cart_path
-  end
-
-  def destroy
-    @order = current_order
-    @item = @order.order_items.find_by(id: params[:id])
+    @item = @order.order_items.find(params[:id])
     unless @item
       head :not_found
       return
     else
-      @item.destroy 
-      # if cart is empty, reset session order_id to be nil 
+      if @item.update_attributes(item_params)
+        flash[:status] = :success
+        flash[:message] = "Quantity successfully updated"
+      else
+        flash[:status] = :error
+        flash[:message] = "Quantity invalid: #{@item.error.messages}"
+      end
+      redirect_to view_cart_path
+    end
+
+    def destroy
+      @order = current_order
+      @item = @order.order_items.find_by(id: params[:id])
+      unless @item
+        head :not_found
+        return
+      end
+      @item.destroy
+      # if cart is empty, reset session order_id to be nil
       if @order.order_items == []
-          session[:order_id] = nil
-          flash[:status] = :success
-          flash[:message] = "Your cart is empty"
-          redirect_to products_path
-          return 
-      end 
+        session[:order_id] = nil
+        flash[:status] = :success
+        flash[:message] = "Your cart is empty"
+        redirect_to products_path
+        return
+      end
       if @order.save
         flash[:status] = :success
         flash[:message] = "Item successfully deleted from cart"
@@ -64,7 +70,7 @@ class OrderItemsController < ApplicationController
         flash[:message] = "There was a problem deleting this item"
       end
       redirect_to view_cart_path
-    end 
+    end
   end
 
   private
@@ -72,5 +78,4 @@ class OrderItemsController < ApplicationController
   def item_params
     params.require(:order_item).permit(:quantity, :product_id)
   end
-    
 end
